534f86e1a260f0abded81e97281723b04bc4ad2a
Toks Fawibe <tafawibe@gmail.com>
2019-08-03 21:08:59 -0500

    Authentication complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt =  require('jsonwebtoken');
 const config = require('config');
+//This middleware gets an authentication token and verifies the token. Ultimately the middleware passes the data to our authentication route.
 
 module.exports  = function(req, res, next) {
     //Get token from header
@@ -8,12 +9,17 @@ module.exports  = function(req, res, next) {
     //Check if no token
     if(!token){
         return res.status(401).json,({msg: 'No token, authorization denied'});
-    }
-}
+    };
 
 //Verify token
-try{
-    const decoded =jwt.verify(token, config.get('jwtSecret'));
-} catch(err){
-    res.status(401).json({msg: 'Token is not valid'});
-}
+    try{
+        const decoded =jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();
+    } catch(err) {
+        res.status(401).json({msg: 'Token is not valid' });
+    };
+};
+
+//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWQ0NjM1NjlhOWNmZTI0Mzc0ZTAzNGY1In0sImlhdCI6MTU2NDg4MjI4MSwiZXhwIjoxNTY1MjQyMjgxfQ.MHUO21OTMWr0EzWxSiE0Fo-1wNhMbOA9VUQyf2tdUxY
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const {check, validationResult} = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
-//@route    GET api/route
+const User = require('../../models/Users')
+
+//@route    GET api/auth
 //@desc     Test route
 //@access   Public
-router.get('/', (req,res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+        
+    } catch (error) {
+        console.error(err.message);
+        res.status();
+    }
+});
+
+//@route    GET api/auth
+//@desc     Authenticate user and get token 
+//@access   Public
+router.post('/', 
+    [
+        check('email', 'Please include a value').isEmail(),
+        check('password', 'password is required').exists()
+    ],
+    async(req,res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+        
+    const{ name, email, password } = req.body; //parsing email from request
+
+    try {
+    //See if users exists
+        let user = await User.findOne({email});
+
+        if (!user){
+            return res.status(400).json({errors : [{ msg: 'Invalid Credentials'}]})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({errors : [{ msg: 'Invalid Credentials'}]});
+        };
+    //Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'),{expiresIn: 360000},  /*after authentication, create login token: pass in secret from default.json, 
+                set expiration to 100hrs, will change to an hour in production*/
+                (err,token)=> {
+                    if (err) throw err;
+                    res.json({ token });
+                    }
+                );
+    
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+    }
+);
 
 module.exports = router;
