6c97039e2ad13dbacb9b3b9869ffb61b96420027
Toks Fawibe <tafawibe@gmail.com>
2019-09-05 10:37:38 -0500

    New changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,25 @@
-const jwt =  require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const config = require('config');
 //This middleware gets an authentication token and verifies the token. Ultimately the middleware passes the data to our authentication route.
 
-module.exports  = function(req, res, next) {
-    //Get token from header
-    const token = req.header('x-auth-token');
+module.exports = function(req, res, next) {
+  //Get token from header
+  const token = req.header('x-auth-token');
 
-    //Check if no token
-    if(!token){
-        return res.status(401).json,({msg: 'No token, authorization denied'});
-    };
+  //Check if no token
+  if (!token) {
+    return res.status(401).json, { msg: 'No token, authorization denied' };
+  }
 
-//Verify token
-    try{
-        const decoded =jwt.verify(token, config.get('jwtSecret'));
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-        req.user = decoded.user;
-        next();
-    } catch(err) {
-        res.status(401).json({msg: 'Token is not valid' });
-    };
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
 };
 
 //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWQ0NjM1NjlhOWNmZTI0Mzc0ZTAzNGY1In0sImlhdCI6MTU2NDg4MjI4MSwiZXhwIjoxNTY1MjQyMjgxfQ.MHUO21OTMWr0EzWxSiE0Fo-1wNhMbOA9VUQyf2tdUxY
