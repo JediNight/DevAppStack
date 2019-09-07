0a2798e20bd042e217d3bdfeb9f3662ccacd8a22
Toks Fawibe <tafawibe@gmail.com>
2019-09-06 19:20:54 -0500

    experience_null_error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,40 +1,25 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
-//This middleware gets an authentication token and verifies the token. Ultimately the middleware passes the data to our authentication route.
+//This middleware gets an authentication token and vWerifies the token. Ultimately the middleware passes the data to our authentication route.
 
 module.exports = function(req, res, next) {
   //Get token from header
   const token = req.header('x-auth-token');
 
-<<<<<<< HEAD
   //Check if no token
   if (!token) {
     return res.status(401).json, { msg: 'No token, authorization denied' };
   }
-=======
-    //Check if no token
-    if(!token){
-        return res.status(401).json({msg: 'No token, authorization denied'});
-    }
->>>>>>> 18336a8d6a750ce1a5a460df1203ad61d29eae93
 
   //Verify token
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-<<<<<<< HEAD
     req.user = decoded.user;
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
-=======
-        req.user = decoded.user;
-        next();
-    } catch(err) {
-        res.status(401).json({msg: 'Token is not valid' });
-    }
->>>>>>> 18336a8d6a750ce1a5a460df1203ad61d29eae93
 };
 
 //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWQ0NjM1NjlhOWNmZTI0Mzc0ZTAzNGY1In0sImlhdCI6MTU2NDg4MjI4MSwiZXhwIjoxNTY1MjQyMjgxfQ.MHUO21OTMWr0EzWxSiE0Fo-1wNhMbOA9VUQyf2tdUxY
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,136 +3,210 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
-const auth = require ('../../middleware/auth');
-const Profile = require ('../../models/Profile');
- // eslint-disable-next-line no-unused-vars
-const User = require ('../../models/Users');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+// eslint-disable-next-line no-unused-vars
+const User = require('../../models/Users');
 
 //@route    GET api/profile/me
 //@desc     Get current users profile
 //@access   Private
-router.get('/me', auth, async (req,res) => {
-    try {  
-        const profile = await Profile.findOne({user: req.user.id}).populate('user',['name','avatar']);
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
 
-        if (!profile){
-            return res.status(400).json({msg: 'There is no profile for this user'});
-        }
-        res.json(profile);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
-
-}
-);
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 //@route    POST api/profile/me
 //@desc     Create or update profile
 //@access   Private
 
-router.post('/', [auth, [
-    check('status', 'Status is required').not().isEmpty(),
-    check('skills', 'Skills is required').not().isEmpty()] // eslint-disable-next-line no-unused-vars
-],async (req,res) => { // eslint-disable-next-line no-unused-vars
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
-        }
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ] // eslint-disable-next-line no-unused-vars
+  ],
+  async (req, res) => {
+    // eslint-disable-next-line no-unused-vars
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-        const {
-            company,
-            website,
-            location,
-            bio,
-            status,
-            githubusername,
-            skills,
-            youtube,
-            facebook,
-            twitter,
-            instagram,
-            linkedin
-        } = req.body
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
 
-        //Build profile object
-        const profileFields = {};
-        profileFields.user = req.user.id;
-        if(company) profileFields.company = company;
-        if(website) profileFields.website = website;
-        if(location) profileFields.location = location;
-        if(bio) profileFields.bio = bio;
-        if(status) profileFields.status = company;
-        if(githubusername) profileFields.githubusername = githubusername;
-        if (skills) {
-            profileFields.skills = skills.split(',').map(skill => skill.trim());
-        }
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = company;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
 
     //    Build social object
-        profileFields.social = {}
-       if(youtube) profileFields.social.youtube = youtube
-       if(twitter) profileFields.social.twitter = twitter
-       if(facebook) profileFields.social.facebook = facebook
-       if(linkedin) profileFields.social.linkedin = linkedin
-       if(instagram) profileFields.social.instagram = instagram
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
 
-        console.log(profileFields.skills);
+    console.log(profileFields.skills);
 
-        try {
-            let profile = await Profile.findOne({user: req.user.id})
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
 
-            if(profile){
-                profile = await Profile.findOneAndUpdate({user: req.user.id}, {$set: profileFields}, {new: true});
-                return res.json(profile);    
-            }
-            profile = new Profile(profileFields);
-            await profile.save();
-            res.json(profile);
-        } catch (error) {
-            console.error(error.message);
-            res.status(500).send('Server Error');
-        }
-
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
     }
+  }
 );
 
 //@route    GET api/profile/me
 //@desc     Get all profile
 //@access   Public
 
-router.get('/', auth, async(req,res) => {
-    try {
-        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
-        res.json(profiles);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
-    }
-
-    }
-)
+router.get('/', auth, async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 //@route    GET api/profile/me
 //@desc     Get all profile
 //@access   Public
 
-router.get('/user/:user_id', auth, async(req,res) => {
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+    if (!profile)
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    DELETE api/profile
+//@desc     Delete user and posts
+//@access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    await Profile.findOneAndDelete({ user: req.user.id });
+    await User.findOneAndDelete({ user: req.params.user_id });
+    res.json({ msg: 'User deleted' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    PUT api/profile
+//@desc     Add Profile Experience
+//@access   Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('from', 'Title is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { title, company, from, to, current, description } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      from,
+      to,
+      current,
+      description
+    };
     try {
-        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar']);
-        if(!profile) return res.status(400).json({msg: 'There is no profile for this user'})
-        res.json(profile)
+      const profile = await Profile.findOne({ user: req.user.id });
 
+      profile.experience.unshift(newExp);
 
+      profile.save();
+
+      res.json({ profile });
     } catch (error) {
-        console.error(error.message);
-        if(error.kind == 'ObjectId'){
-            return res.status(400).json({msg: 'Profile not found'})
-        }
-        res.status(500).send('Server Error');
+      console.error(error.message);
+      res.status(500).send('Server Error');
     }
-
-    }
-)
-
+  }
+);
 
 module.exports = router;
